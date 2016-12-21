0c4769d1d0ee0ecaebe1c1af3269dac9b2c57e36
resethread <resethread@gmail.com>
2016-12-21 01:30:59 +0100

    change title content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>cellC0De - blog d'un codeur</title>
+    <title>blogstuff.io - blog d'un codeur</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
-    <link rel="stylesheet" href="">
 
     <!-- Scripts -->
     <script>
@@ -20,6 +19,15 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+
+    <style>
+    #footer {
+            background-color: #222;
+            border-color: #090909;
+            color: #f5f8fa;
+            padding: 3rem 0;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -52,58 +60,79 @@
         </nav>
 
         <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        @yield('content')
+            <div class="row">
+                <div class="col-md-8">
+                    @yield('content')
+                </div>
+                <div class="col-md-4">
+                    <div class="well">
+                        <h4>Recherche</h4>
+                        <div class="input-group">
+                            <input type="text" class="form-control">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                            </span>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="well">
-                            <h4>Recherche</h4>
+                    <div class="well">
+                        <h4>Categories</h4>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                @php
+                                    $categories = ['Tor', 'VPN', 'Cryptographie', 'Sécurité']
+                                @endphp
+                                <ul class="list-unstyled">
+                                    
+                                    @foreach($categories as $category)
+                                        <li><a href="#">{{ $category }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                @php
+                                    $categories = ['Politique', 'Censure', 'Surveillance', 'Autre']
+                                @endphp
+                                <ul class="list-unstyled">
+                                    @foreach($categories as $category)
+                                        <li><a href="#">{{ $category }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="well">
+                        <h4>Side Widget Well</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+                    </div>
+                    <div class="well">
+                        <h4>Abonnez vous à la newsletter <br> <small>on spam pas promis</small></h4>
+                        <form action="/newsletter" method="POST">
+                            {{ csrf_field() }}
                             <div class="input-group">
-                                <input type="text" class="form-control">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button">
-                                        <span class="glyphicon glyphicon-search"></span>
-                                </button>
+                                <input type="text" class="form-control" placeholder="Search for...">
+                                    <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">Envoie</button>
                                 </span>
                             </div>
-                        </div>
-                        <div class="well">
-                            <h4>Categories</h4>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    @php
-                                        $categories = ['Tor', 'VPN', 'Cryptographie', 'Sécurité']
-                                    @endphp
-                                    <ul class="list-unstyled">
-                                        
-                                        @foreach($categories as $category)
-                                            <li><a href="#">{{ $category }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                                <div class="col-lg-6">
-                                    @php
-                                        $categories = ['Politique', 'Censure', 'Surveillance', 'Autre']
-                                    @endphp
-                                    <ul class="list-unstyled">
-                                        @foreach($categories as $category)
-                                            <li><a href="#">{{ $category }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="well">
-                            <h4>Side Widget Well</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+        
+        
+        <footer id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        {{ date('Y') }} - Le contenu du blog est en licence WTFPL
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
-
     <!-- Scripts -->
     <script src="/js/app.js"></script>
 </body>
