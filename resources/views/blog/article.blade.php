0e07afb560d41642b119c7d22aabb00e301f7137
netwarp <resethread@gmail.com>
2017-01-19 16:25:57 +0100

    add form for comment

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -9,14 +9,67 @@
 		{!! $article->overview !!}
 		<br>
 		{!! $article->content !!}
+		<hr>
 		<div>
-			{{ $article->created_at }}
+			{{ date('d F Y', strtotime($article->created_at)) }}
 		</div>
 	</div>
 </div>
-<script>
-    document.querySelectorAll('#article-content img').forEach(function(image) {
-        image.classList.add('img-responsive')
-    })
-</script>
+
+<div class="panel panel-default" id="form-commment">
+	<div class="panel-heading"><span class="h4">Commentaires</span></div>
+	<div class="panel-body">
+		@if (session('success'))
+			<div class="alert alert-success">
+				{{ session('success') }}
+			</div>
+		@else
+			<div class="alert alert-info">
+				Bien que nous soyons partisans d'une liberté d'expression totale, nous rappelons à nos intervenants que l'éspace de commentaires n'est pas un défouloire et que la courtoisie y est de mise. <br>
+				La modération.
+			</div>
+		@endif
+		<form action="/article/{{ $article->id }}" method="POST">
+			{{ csrf_field() }}
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="name">Nom</label>
+					<input type="text" class="form-control" name="name" placeholder="Nom">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="name">Email</label>
+					<input type="text" class="form-control" name="email" placeholder="Email">
+				</div>
+			</div>
+			<div class="col-md-12">
+				<label for="message">Contenu</label>
+				<textarea name="content" class="form-control" rows="12"></textarea>
+			</div>
+			<div class="col-md-12">
+				<br>
+				<button type="submit" class="btn btn-success pull-right">Envoie</button>
+			</div>
+		</form>
+	</div>
+</div>
+
+@foreach($comments as $comment)
+	<div class="jumbotron">
+		<div class="container-fluid">
+			<div >
+				<b>{{ $comment->name }}</b>
+				<small>&nbsp; {{ date('d F Y', strtotime($article->created_at)) }}</small>
+			</div>
+			<div>
+				{{ $comment->content }}
+			</div>
+			<div>
+				<a href="#form-commment" class="btn btn-primary btn-sm pull-right">Répondre</a>
+			</div>
+		</div>
+	</div>
+@endforeach
+
 @endsection
