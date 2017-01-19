a0a57ebd46924ecb260d747817771d6b3a2a9c37
netwarp <resethread@gmail.com>
2017-01-19 16:27:02 +0100

    many fix

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,17 +13,56 @@ class BlogController extends Controller {
     }
 
     public function getIndex() {
+
+
     	$articles = DB::table('articles')->orderBy('id', 'desc')->paginate(4);
 
     	return view('blog.index', compact('articles'));
     }
 
+    public function getSearch(Request $request) {
+        $word = $request->input('word');
+
+
+        return view('blog.index');
+    }
+
     public function getArticle($id, $slug) {
         $article = DB::table('articles')->where('id', $id)->first();
 
-        $comments = app('db')->select("SELECT * FROM comments WHERE article_id = $id ORDER BY id DESC");
-     //   $botblocker = new Botblocker;
-        return view('blog.article', compact('article', 'comments'));
+        if ($article) {
+            $comments = app('db')->select("SELECT * FROM comments WHERE article_id = $id ORDER BY id DESC");
+
+           return view('blog.article', compact('article', 'comments'));
+        }
+
+        
+    }
+
+    public function postComment($id, Request $request) {
+
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'content' => 'required'
+        ]);
+        
+        DB::table('comments')->insert([
+            'article_id' => $id,
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'content' => $request->input('content'),
+            'ip' => sha1($request->ip()),
+            'created_at' => Carbon::now()
+        ]);
+
+        return redirect()->back()->with('success', 'Votre commentaire a bien été envoyé');
+    }
+
+    public function postNewsletters(Request $request) {
+        $email = $request->input('email');
+
+        dd($email);
     }
 
     public function getProjets() {
@@ -54,5 +93,9 @@ class BlogController extends Controller {
         ]);
 
         return redirect()->back()->with('success', 'Votre message a été envoyé');
+    }
+
+    public function getTest() {
+        return view('blog.home');
     }
 }
