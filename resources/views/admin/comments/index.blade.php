89fc89baade2922e2cf38d0d2c1d60b16ae9b8b9
netwarp <resethread@gmail.com>
2017-01-19 16:45:35 +0100

    comments views

---
+++ resources/views/admin/comments/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="col s12">
+    <nav class="blue-grey darken-2">
+        <div class="nav-wrapper">
+            <div class="col s12">
+                <a href="#!" class="breadcrumb">Admin</a>
+                <a href="#!" class="breadcrumb">Comments</a>
+            </div>
+        </div>
+    </nav>
+</div>
+<div class="row">
+    <div class="col s12">
+        <div class="card">
+            <div class="card-content">
+                <table class="bordered stripped">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Content</th>
+                            <th>Article_id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($comments as $comment)
+                        <tr>
+                            <td>{{ $comment->id }}</td>
+                            <td><a href="{{ route('admin.comments.show', $comment->id) }}">{{ $comment->name }}</a></td>
+                            <td>{{ $comment->email }}</td>
+                            <td>{{ $comment->content }}</td>
+                            <td><a href="/admin/articles/{{ $comment->article_id }}">{{ $comment->article->title }}</a></td>
+                            <td>
+                                <form action="{{ route('admin.comments.destroy', $comment->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="_method" value="DELETE">
+
+                                    <button type="submit" class="btn red darken-3"><i class="trash outline icon"></i> Supprimer</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/comments/show.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="col s12">
+        <nav class="blue-grey darken-2">
+            <div class="nav-wrapper">
+                <div class="col s12">
+                    <a href="#!" class="breadcrumb">Admin</a>
+                    <a href="#!" class="breadcrumb">Comments</a>
+                    <a href="#!" class="breadcrumb">Show</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <h4>Comments</h4>
+                    <div>
+                        <b>Name</b>:
+                        {{ $comment->name }}
+                    </div>
+                    <div>
+                        <b>Email</b>:
+                        {{ $comment->email }}
+                    </div>
+                    <div>
+                        <b>Content</b>:
+                        {{ $comment->content }}
+                    </div>
+                    <div>
+                        <b>Article</b>:
+                        {{ $comment->article_id }}
+                    </div>
+                    <div>
+                        <b>Date</b>:
+                        {{ $comment->created_at }}
+                    </div>
+                    <div>
+                        <b>Ip</b>:
+                        {{ $comment->ip }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
