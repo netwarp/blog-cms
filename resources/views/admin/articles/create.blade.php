99ead4aa3b398c00f8b5faa81f209b05fbeb64e8
netwarp <resethread@gmail.com>
2017-01-25 18:40:19 +0100

    try fix 2

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -10,10 +10,19 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.3.10/plugins/codesample/plugin.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.3.10/plugins/bbcode/plugin.js"></script>
 
-
-
-    <script src="/js/admin/articles_forms.js"></script> 
-
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'paste fullscreen codesample code image media',
+            paste_data_images: true,
+            images_upload_url: '/api/upload',
+            images_upload_base_path: '/api/upload',
+            location : '/api/upload',
+            menubar: true,
+            content_style: ".mce-content-body  {font-size: 14px;}",
+            toolbar: 'undo redo bold italic alignleft aligncenter alignright codesample fullscreen code image media'
+        })
+    </script>
 
     <div class="col s12">
         <nav class="blue-grey darken-2">
