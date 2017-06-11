211e302806fa13be8874ed1aa8471b9acfdd5ab7
netwarp <resethread@gmail.com>
2017-06-11 17:25:03 +0200

    rename page project -> tools

--- resources/views/blog/projets.blade.php
+++ resources/views/blog/projets.blade.php
@@ -2,8 +2,36 @@
 @section('content')
     <div class="panel panel-default">
     	<div class="panel-body">
-    		<h1>Projets</h1>
-    		Stay tunned...
+    		<h1>Outils</h1>
+    		<div>
+    			Sur cette page vous avez plein d'outils utile pour limiter votre flicage sur le web:
+    		</div>
+
+    		<h2>Remplacer Google</h2>
+
+    		<p>Si vous ne le savez pas Google enregistre et analyse absoluement tout ce que vous faites avec leurs services. Vos recherches, vos mails, vos calendriers, tout y passe. Et comme si ça ne suffisait pas, cette société contribue à la censure d'internet</p>
+			
+			<h3>Remplacer la recherche</h3>
+
+    		<ul class="list-unstyled">
+    			<li><a href="http://duckduckgo.com" target="_blank">Duckduckgo</a></li>
+    		</ul>
+
+    		<h3>Remplacer gmail</h3>
+    		<ul class="list-unstyled">
+    			<li><a href="#" target="_blank">Protonmail</a></li>
+    			<li><a href="#" target="_blank">Tutanota</a></li>
+    			<li><a href="#" target="_blank">Inboen</a></li>
+    		</ul>
+
+    		<h3>Remplacer chrome</h3>
+    		<ul class="list-unstyled">
+    			<li><a href="#" target="_blank">Firefox</a></li>
+    			<li><a href="#" target="_blank">Tor browser</a></li>
+    		</ul>
+
+    		<h2>Contourner les boites noires</h2>
+
     	</div>
     </div>
 @endsection
